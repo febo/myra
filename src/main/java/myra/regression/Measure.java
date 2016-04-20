64bacefe8ec1c73a2e036fc23734de87c9c31fe6
febo <febo@kent.ac.uk>
2016-04-20 23:01:52 +0100

    Base class for regression measures

---
+++ src/main/java/myra/regression/Measure.java
@@ -0,0 +1,50 @@
+/*
+ * Measure.java
+ * (this file is part of MYRA)
+ * 
+ * Copyright 2008-2016 Fernando Esteban Barril Otero
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package myra.regression;
+
+import myra.Cost;
+import myra.data.Dataset;
+import myra.data.Model;
+
+/**
+ * Base class for model regression evaluation measures.
+ * 
+ * @since 4.5
+ * 
+ * @author Fernando Esteban Barril Otero
+ */
+public abstract class Measure extends myra.data.Measure {
+    @Override
+    public Cost evaluate(Dataset dataset, Model model) {
+	return evaluate(dataset, (RegressionModel) model);
+    }
+
+    /**
+     * Returns the quality of the specified classification model.
+     * 
+     * @param dataset
+     *            the current dataset.
+     * @param model
+     *            the classification model to evaluate.
+     * 
+     * @return the quality of the specified classification model.
+     */
+    public abstract Cost evaluate(Dataset dataset, RegressionModel model);
+}
